ba5f46464e6a4fae5ad019d3db54197b2efae0b3
Ekta Patel <architectekta@gmail.com>
2023-05-09 22:34:40 +0530

    add

---
+++ ETL/etl.py
@@ -0,0 +1,116 @@
+import pandas as pd
+import os
+import glob
+from datetime import datetime
+import json
+import csv
+import math
+import shutil
+
+# define the paths to the raw_files directory and the status.csv file
+raw_files_path = "C:/Users/archi/OneDrive/Desktop/ETL/row_files"
+processed_files_path = "C:/Users/archi/OneDrive/Desktop/ETL/processed_files"
+status_file_path = "C:/Users/archi/OneDrive/Desktop/ETL/status/status.csv"
+
+processed_files = set()
+
+# populate the processed_files set with existing CSV files
+for filename in os.listdir(processed_files_path):
+    if filename.endswith('.csv'):
+        processed_files.add(filename[:-4])
+
+# load the existing status.csv file if it exists, or create a new dataframe if it doesn't
+if os.path.exists(status_file_path):
+    # check if the file is empty
+    if os.path.getsize(status_file_path) > 0:
+        status_df = pd.read_csv(status_file_path)
+    else:
+        status_df = pd.DataFrame(columns=["file name", "process time"])
+else:
+    status_df = pd.DataFrame(columns=["file name", "process time"])
+
+# get a list of all the unprocessed json files in the raw_files directory
+unprocessed_files = glob.glob(os.path.join(raw_files_path, "*.json"))
+unprocessed_files = [f for f in unprocessed_files if os.path.basename(f) not in status_df["file name"].tolist()]
+
+# process each unprocessed file and update the status dataframe
+for file_path in unprocessed_files:
+        filename = os.path.basename(file_path)
+    
+        with open(os.path.join(raw_files_path, filename)) as f:
+            data = json.load(f)
+
+        # Extract relevant information
+        season = data['info']['season']
+        toss_winner = data['info']['toss']['winner']
+        toss_decision = data['info']['toss']['decision']
+        teams = ','.join(data['info']['teams'])
+        city = data['info']['city']
+        venue = data['info']['venue']
+        gender = data['info']['gender']
+        match_type = data['info']['match_type']
+
+        # Flatten the JSON file
+        rows = []
+        ball_no = 0  # initialize ball number
+        over_no = 0  # initialize over number
+        for inning in data['innings']:
+            for over in inning['overs']:
+                for delivery in over['deliveries']:
+                    extras = delivery.get('extras', {})
+                    ball_no += 1  # increment ball number by 1 for each delivery
+                    if ball_no % 6 == 1:
+                        over_no += 1
+                    if over_no <= 20:
+                        over_no_str = str(over_no)
+                    else:
+                        over_no_str = str(int(over_no) - 20) + '.' + '0'*(1-math.floor(math.log10(int(over_no) - 19)))
+                    row = {
+                        'match_number': data['info']['event']['match_number'],
+                        'season': season,
+                        'city': city,
+                        'venue': venue,
+                        'gender': gender,
+                        'match_type': match_type,
+                        'toss_winner': toss_winner,
+                        'toss_decision': toss_decision,
+                        'teams': teams,
+                        'team': inning['team'],
+                        'over': over_no_str,
+                        'ballno': ball_no,
+                        'batter': delivery['batter'],
+                        'bowler': delivery['bowler'],
+                        'non_striker': delivery['non_striker'],
+                        'runs_batter': delivery['runs']['batter'],
+                        'runs_extras': sum(extras.values()),
+                        'runs_total': delivery['runs']['total'],
+                        'legbyes': extras.get('legbyes', 0),
+                        'wides': extras.get('wides', 0),
+                        'noballs': extras.get('noballs', 0),
+                        'byes': extras.get('byes', 0)
+                    }
+                    rows.append(row)
+
+        # Write the flattened data to a CSV file
+        csv_filepath = os.path.join('row_files', filename[:-5] + '.csv')
+        print('Creating CSV file:', csv_filepath)
+        with open(csv_filepath, 'w', newline='') as f:
+            writer = csv.DictWriter(f, fieldnames=rows[0].keys())
+            writer.writeheader()
+            writer.writerows(rows)
+
+        # move the CSV file to the processed_files folder
+        shutil.move(csv_filepath, os.path.join(processed_files_path, filename[:-5] + '.csv'))
+
+        # update the set of processed files
+        processed_files.add(filename[:-5])
+         
+        # get the current date and time
+        processtime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        
+        # add the file and process time to the status dataframe
+        status_df = status_df.append({"file name": filename, "process time": processtime}, ignore_index=True)
+
+#save the updated status dataframe to the status.csv file
+status_df.to_csv(status_file_path, index=False)
+            
--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# ETL_Cricket
+### ETL Pipeline for IPL Cricket Data
+This Python script extracts data from JSON files containing Indian Premier League (IPL) cricket match data, flattens the data, and saves it to CSV files. It also updates a status.csv file to keep track of the files that have been processed and when they were processed.
+
+### Libraries Used
+1. pandas: data manipulation and analysis library
+2. os: operating system library
+3. glob: pathname pattern matching library
+4. datetime: library to work with dates and times
+5. json: library to work with JSON data
+6. csv: library to work with CSV files
+7. math: library to work with mathematical functions
+8. shutil: library for file operations
+
+### Variables
+1. raw_files_path: path to the raw JSON files directory
+2. processed_files_path: path to the processed CSV files directory
+3. status_file_path: path to the status.csv file
+4. processed_files: a set to store the names of the processed files
+
+### Workflow
+1. Initialize the processed_files set by populating it with the names of the existing CSV files in the processed_files_path directory.
+2. Load the existing status.csv file if it exists, or create a new dataframe with columns "file name" and "process time" if it doesn't.
+3. Get a list of all the unprocessed JSON files in the raw_files_path directory.
+For each unprocessed file:
+4. Extract the relevant information from the JSON file.
+5. Flatten the JSON file and write the flattened data to a CSV file.
+6. Move the CSV file to the processed_files_path directory.
+7. Add the file name and process time to the status_df dataframe.
+8. Save the updated status_df dataframe to the status.csv file.
+
+Overall, this code automates the process of converting JSON files to CSV files and maintains a record of the processed files.
